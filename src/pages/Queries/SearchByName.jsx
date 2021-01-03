ece4440e5c1ac86afa78d7633f5fe2f58ae122f8
Moreonenight <40230165+Moreonenight@users.noreply.github.com>
2021-01-03 13:45:36 +0800

    第一个能用的页面

--- src/pages/Queries/SearchByName.jsx
+++ src/pages/Queries/SearchByName.jsx
@@ -1,71 +1,61 @@
-import { Button, message, Input, Drawer, Card, Col, Row, Divider, Table } from 'antd';
-import React, { useState, useRef, useEffect } from 'react';
-import { useIntl, FormattedMessage } from 'umi';
-import { PageContainer, FooterToolbar } from '@ant-design/pro-layout';
-import { Chart, Interval, Line, Point, Tooltip, Axis, useView, Coordinate } from 'bizcharts';
+import { Input, Card, Divider, Table } from 'antd';
+import React, { useState } from 'react';
+import { PageContainer } from '@ant-design/pro-layout';
+import { Chart, Interval, Coordinate } from 'bizcharts';
+import request from '@/utils/request';
 const { Search } = Input;
 
 const SearchByName = () => {
-
   const [dataList, setDataList] = useState([]);
 
-  const [starData, setStarData] = useState([]);
+  const [timeData, setTimeData] = useState([
+    {
+      数据库: "MySQL",
+      执行时间: 0
+    },
+    {
+      数据库: "Neo4j",
+      执行时间: 0
+    },
+    {
+      数据库: "Hive",
+      执行时间: 0
+    },
+  ]);
 
-  const [dailyData, setDailyData] = useState([]);
+  const [counter, setCounter] = useState(0);
 
-  // useEffect(() => {
-  //   getSalesAnalysis().then((res) => {
-  //     if (!res.data) return;
-  //     let newDailyData = [];
-  //     const curDate = new Date();
-  //     for (let i = res.data.length - 1; i >= 0 ; i--) {
-  //       let historyDate = new Date(curDate.getTime() - 24 * 60 * 60 * 1000 * i);
-  //       let month = historyDate.getMonth() + 1;
-  //       let date = historyDate.getDate();
-  //       newDailyData.push({
-  //         date: month + '-' + date,
-  //         销售额: res.data[i],
-  //       })
-  //     }
-  //     setDailyData(newDailyData);
-  //   })
-  // }, []);
-
-  // useEffect(
-  //   () => {
-  //     getTopSellingItem().then((res) => {
-  //       if (!res.data) return;
-  //       let newStarData = [];
-  //       for (let i = 0; i < res.data.length; i++) {
-  //         newStarData.push({
-  //           source: '商品 ' + res.data[i].name,
-  //           金额: res.data[i].total,
-  //         })
-  //       }
-  //       getBestCustomer().then((res) => {
-  //         if (!res.data) return;
-  //         for (let i = 0; i < res.data.length; i++) {
-  //           newStarData.push({
-  //             source: '用户 ' + res.data[i].name,
-  //             金额: res.data[i].total,
-  //           })
-  //         }
-  //         console.log(newStarData);
-  //         setStarData(
-  //           newStarData
-  //         )
-  //       })
-  //     })
-  //   }, []
-  // )
-
-  // useEffect(
-  //   () => {
-  //     getAllOrder().then((res) => {
-  //       setDataList(res.data);
-  //     })
-  //   }, []
-  // );
+  const SearchMovieName = value => {
+    if (value)
+      request('/api/title' + '?' + 'Title=' + value).
+        then((res) => {
+          console.log(res);
+          if (res.data) {
+            setCounter(res.data.count);
+            setTimeData([
+              {
+                数据库: "MySQL",
+                执行时间: res.data.MySqlTime
+              },
+              {
+                数据库: "Neo4j",
+                执行时间: res.data.Neo4jTime
+              },
+              {
+                数据库: "Hive",
+                执行时间: res.data.HiveTime
+              },
+            ]);
+            setDataList(res.data.movieDatas.map((movie) => ({
+              id: movie.id,
+              title: movie.Title,
+              videoTime: movie.videoTime,
+              points: movie.points,
+              totalNumber: movie.totalNumber
+            })))
+          }
+        })
+  }
 
   const columns = [
     {
@@ -98,26 +88,10 @@ const SearchByName = () => {
     },
   ];
 
-  const time_data = [
-    {
-      数据库: "MySQL",
-      执行时间: 131744
-    },
-    {
-      数据库: "Neo4j",
-      执行时间: 104970
-    },
-    {
-      数据库: "Hive",
-      执行时间: 29034
-    },
-  ];
-  const counter = 0
-
   return (
     <PageContainer>
       <Card title="执行时间（毫秒）" style={{ width: '100%' }}>
-        <Chart height={150} data={time_data} autoFit>
+        <Chart height={150} data={timeData} autoFit>
           <Coordinate transpose />
           <Interval position="数据库*执行时间" />
         </Chart>
@@ -128,7 +102,9 @@ const SearchByName = () => {
         addonBefore={`当前结果总数：${counter}`}
         allowClear
         enterButton="开始查询"
-        size="large" />
+        size="large"
+        onSearch={SearchMovieName}
+      />
       <Divider />
       <Table
         dataSource={dataList}
